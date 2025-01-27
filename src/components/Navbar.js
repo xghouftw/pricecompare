a0939f11cf0ca1e90031432ac9abdda7d47d3fd2
XGHouE <connor.b.zhang.27@dartmouth.edu>
2025-01-27 17:40:40 -0500

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-  return (
-      <nav className = "nav">
-          <div className="left">
-              <img src="/supermonkey.webp" alt="Logo" className="logo" />
-              <h1 className="title">Supermonkey's Supermarket Comparison</h1>
-          </div>
-          <ul className="right">
-              <li><Link to="/change-location">Change Location</Link></li>
-              <li><Link to="/add-item">Add Item</Link></li>
-              <li><Link to="/see-cart">See Cart</Link></li>
-          </ul>
-      </nav>
-  );
+    return (
+        <div className = "nav">
+            <div className="left">
+                <img src="/supermonkey.webp" alt="Logo" className="logo" />
+                <h1 className="title">Supermonkey's Supermarket Comparison</h1>
+            </div>
+            <ul className="right">
+                <li><Link to="/change-location">Change Location</Link></li>
+                <li><Link to="/add-item">Add Item</Link></li>
+                <li><Link to="/see-cart">See Cart</Link></li>
+            </ul>
+        </div>
+    );
 }
 
 export default Navbar;
